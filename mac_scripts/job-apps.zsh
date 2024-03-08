6222cde810f77c96a5401b7c4677298bebd5bea8
Andrew Tischhauser <andrewtisc@hotmail.com>
2024-03-07 21:40:05 -0600

    Add job-apps.zsh

---
+++ mac_scripts/job-apps.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# AUTHOR: Andrew Tischhauser
+# DESCRIPTION: Opens all of the websites and folders I need to apply to jobs.
+
+open "~/OneDrive/Job Apps/Resumes"
+open -a Safari "https://www.linkedin.com/jobs/"
+open -a Safari "https://www.glassdoor.com/Job/index.htm"
+open -a Safari "https://www.indeed.com"
